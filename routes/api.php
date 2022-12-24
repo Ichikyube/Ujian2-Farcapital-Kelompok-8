11c7496118b35e994164d5fbb4ea415d4be7eab7
maulana48 <hidarizero4869@gmail.com>
2022-12-24 08:53:52 +0700

    api-aspirasi

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\{ AspirasiController };
+ 
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +19,15 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::prefix('Aspirasi')
+    ->name('aspirasi.')
+    ->controller(AspirasiController::class)
+    ->group(function(){
+        Route::get('/', 'index')->name('list');
+        Route::get('/{aspirasi}', 'show')->name('show');
+        
+        Route::post('/', 'store')->name('store');
+        Route::post('/update/{aspirasi}', 'update')->name('update');
+        Route::post('/{aspirasi}', 'destroy')->name('destroy');
+    });
